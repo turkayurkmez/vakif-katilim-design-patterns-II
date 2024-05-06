5b49dd1fe551c588f6472327ed4ac0094ab9de3b
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-06 12:27:59 +0300

    Contstructor explained

---
+++ OOPFundamentals/Constructor/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Constructor;
+
+Console.WriteLine("Hello, World!");
+
+Report report = new Report("C:\\Users\\turka\\OneDrive\\Belgeler\\data.xlsx");
+Console.WriteLine(report.ReportType);
+
+Report salesReport = new Report("C:\\Users\\turka\\OneDrive\\Belgeler\\data.xlsx", "HTML");
+
---
+++ OOPFundamentals/Constructor/Report.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Report : IDisposable
+    {
+        public string ReportType { get; set; }
+        public string ExcelFilePath { get; set; }
+
+        public Report(string path): this(path,"PDF")
+        {
+          
+         
+
+        }
+
+        public Report(string path, string reportType)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"{path} adresinde dosya bulunamadı!");
+            }
+            ExcelFilePath = path;
+            ReportType = reportType;
+        }
+
+        public void Dispose()
+        {
+
+            ExcelFilePath = string.Empty;
+            ReportType = string.Empty;
+        }
+    }
+}
