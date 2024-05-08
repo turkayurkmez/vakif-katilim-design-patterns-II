ede02ba87884033f84d8f30a2351913c2a01ab4a
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-08 15:37:17 +0300

    Adapter Design Pattern

---
+++ AllInOneDesignPattern/Structural/Adapter/DesignPattern.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    /*
+     * Sistem harici bir bileşenle bilgi alışverişi yapmaktadır. Ancak harici bileşen, size istediğinizden farklı türde veri göndermektedir. Bu problemi nasıl çözersiniz
+     * 
+     * 
+     */
+    //1. Client: Eski ve değişmesi mümkün olmayan iş mantığı burada
+    //2. Client Interface: diğer sınıfların beklentisiyle iş birliği yapabilecek arayüz.
+    //3. Service: Harici bileşende çalışmasını istediğiniz fonksiyon.
+    //4. Adaptör: Serviste olanı client'in istediğine dönüştüren yapı
+
+
+    //Senaryo:
+    /*
+     *  Eski yazılmış bir obje;  geriye XML dönmektedir.
+     *  Ancak bizim uygulamamıza JSON gerekmekte.
+     *  *  
+     */
+
+
+    //2.:
+
+    public interface IDataTransfer
+    {
+        public string GetData();
+      
+    }
+
+    //3. 
+    public class XMLSource : IDataTransfer
+    {
+        public string GetData()
+        {
+            return "XML Verisi....";
+        }
+    }
+
+    //4.:
+    public class JsonAdapter : IDataTransfer
+    {
+        private readonly XMLSource adaptee;
+        public JsonAdapter(XMLSource source)
+        {
+            this.adaptee = source;
+        }
+        public string GetData()
+        {
+            return adaptee.GetData();
+        }
+    }
+
+}
--- AllInOneDesignPattern/Structural/Adapter/Program.cs
+++ AllInOneDesignPattern/Structural/Adapter/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Adapter;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+XMLSource source = new XMLSource();
+JsonAdapter adapter = new JsonAdapter(source);
+
+Console.WriteLine(adapter.GetData());
+//DataAdapter dataAdapter = new SqlDataAdapter()
+//DataSet dataTable = new DataSet();
+//dataAdapter.Fill(dataTable);
