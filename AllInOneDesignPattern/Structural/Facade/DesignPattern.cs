3ed6024e723dfb2e85cdeb0ad9e2c5ecafaf9f6b
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-09 15:07:14 +0300

    Facade & Flyweight completed

---
+++ AllInOneDesignPattern/Structural/Facade/DesignPattern.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    /*
+     * Çok fazla nesne tarafından yapılacak çok karmaşık süreçlerden oluşan bir işi nasıl basitleştirirsiniz? 
+     * 
+     * Çözüm:
+     * 
+     * Karmaşık işleri arkaya itip, bir sınıf ile "cephe" oluşturursunuz.
+     *  
+     */
+
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+    }
+
+    public class Order
+    {
+        public Order()
+        {
+            Id = 8;
+        }
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public DateOnly OrderDate { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+    //Kişisel not: elbette "facade" kelimesi sınıfta olmamalı ama işin önemli kısmını belirtmek için sembolik olarak bu ismi verdim:
+    public class OrderServiceFacade
+    {
+        
+        public void CreateOrder(Customer customer, List<Product> products, int quantity)
+        {
+            Order order = new Order();
+            order.Customer = customer;
+
+            List<OrderItem> orderItems = new List<OrderItem>();
+
+         
+
+            products.ForEach(p =>
+            {
+                orderItems.Add(new OrderItem { OrderId = order.Id, ProductId = p.Id, Quantity = quantity });
+                Console.WriteLine($"{customer.Name}, {order.OrderDate} tarihinde, {p.Name} isimli ürünü satın aldı");
+                Console.WriteLine($"{p.Name} isimlü ürünün stoğundan {quantity} adet düşüldü");
+            });
+          
+
+
+
+
+
+        }
+    }
+
+
+
+
+}
---
+++ AllInOneDesignPattern/Structural/Facade/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Facade;
+
+Console.WriteLine("Hello, World!");
+
+OrderServiceFacade orderService =  new OrderServiceFacade();
+Customer customer = new Customer { Name = "Türkay" };
+List<Product> products = new()
+{
+    new(){ Id=1, Name="Ürün 1", Stock = 100},
+    new(){ Id=2, Name="Ürün 2", Stock = 100},
+    new(){ Id=3, Name="Ürün 3", Stock = 100},
+
+};
+
+
+orderService.CreateOrder(customer, products, 2);
