408cd3fe9f1abb5be1aea99644cdd720a536f3f2
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-09 12:27:22 +0300

    Decorator pattern completed

---
+++ AllInOneDesignPattern/Structural/Decorator/DesignPattern.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    /*
+     * Problem:
+     *    
+     *    Çekirdek bir nesneye, sürekli yeni işlev veya özellik kazandırıyoruz. Özellikler kombine olabileceğinden miras alamıyorsunuz. Bellekte zaten var olan nesneye özelliği nasıl eklersiniz.
+     *    
+     *    
+     */
+
+    public interface IMail
+    {
+        void Send();
+    }
+
+    public class Mail : IMail
+    {
+        public void Send()
+        {
+            Console.WriteLine("Mail oluşturuldu ve gönderildi");
+        }
+    }
+
+    public abstract class MailDecorator : IMail
+    {
+        protected readonly IMail mail;
+
+        public MailDecorator(IMail mail)
+        {
+            this.mail = mail;
+        }
+        public virtual void Send()
+        {
+          mail.Send();
+        }
+    }
+
+    public class SignedMail : MailDecorator
+    {
+        public SignedMail(IMail mail) : base(mail)
+        {
+        }
+
+        public void SignAndSend()
+        {
+            Console.WriteLine("Mail imzaiandı");
+            this.mail.Send();
+        }
+
+    }
+
+    public class CryptedMail : MailDecorator
+    {
+        public CryptedMail(IMail mail) : base(mail)
+        {
+        }
+
+        public void CryptedSend()
+        {
+            Console.WriteLine("Eposta şifrelendi");
+            this.mail.Send();
+        }
+    }
+
+    public class ArchivedMail : MailDecorator
+    {
+        public ArchivedMail(IMail mail) : base(mail)
+        {
+        }
+
+        public void Archive()
+        {
+            Console.WriteLine("Mail arşive taşındı");
+        }
+    }
+}
---
+++ AllInOneDesignPattern/Structural/Decorator/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator;
+using System.IO.Compression;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World!");
+
+FileStream fileStream = null;
+MemoryStream stream = null;
+NetworkStream networkStream = null;
+
+//GZipStream gZipStream = new GZipStream(fileStream, CompressionMode.Compress);
+//CryptoStream cryptoStream = new CryptoStream(stream, null, CryptoStreamMode.Write);
+
+
+Mail mail = new Mail();
+SignedMail signedMail = new SignedMail(mail);
+signedMail.SignAndSend();
+CryptedMail cryptedMail = new CryptedMail(signedMail);
+cryptedMail.CryptedSend();
+
+ArchivedMail archivedMail = new ArchivedMail(cryptedMail);
+archivedMail.Archive();
+
+
