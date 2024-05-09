e1a0e04bfcb44535a352e92f487651b57363fd57
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-09 09:45:03 +0300

    Bridge pattern explained

---
+++ AllInOneDesignPattern/Structural/Bridge/DesignPattern.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /*
+     * Problem:
+     *  Dikey genişleme (implementasyon), eğer hatalı tasarlanırsa; inheritance kaosu meydana gelebilir. Bu nasıl önlenir
+     *  
+     *  Çözüm:
+     *  is a / has a karşılaştırması
+    */
+
+    public class Report
+    {
+        public ReportType ReportType { get; set; }
+    }
+
+    public class ReportType
+    {
+
+    }
+    public class PDFReport: ReportType
+    {
+
+    }
+    public class ExcelReport: ReportType
+    {
+
+    }
+
+    public class SalesReport: Report    
+    {
+        
+    }
+
+    public class FinanceReport: Report
+    {
+
+    }
+
+    //public class FinancePdfReport: FinanceReport
+    //{
+        
+    //}
+    //public class FinanceExcelReport : FinanceReport
+    //{
+
+    //}
+
+    //public class SalesPDFReport : SalesReport
+    //{
+
+    //}
+
+    //public class SalesExcelReport : SalesReport
+    //{
+
+    //}
+
+    //public class SalesHtmlReport: SalesReport
+    //{
+
+    //}
+}
---
+++ AllInOneDesignPattern/Structural/Bridge/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bridge;
+
+Console.WriteLine("Hello, World!");
+SalesReport salesReport = new SalesReport();
+salesReport.ReportType = new PDFReport();
+salesReport.ReportType = new ExcelReport();
